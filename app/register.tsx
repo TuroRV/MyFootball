e6e7db4aae7c81be1aa888f0678bcc83f7314b42
Javier Arturo Raña Varela <turo@hotmail.es>
2025-10-09 11:54:19 +0200

    crear página de registro

---
+++ app/register.tsx
@@ -0,0 +1,83 @@
+import Button from "@/components/Button";
+import FormInput from "@/components/FormInput";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { router } from "expo-router";
+import { useForm } from "react-hook-form";
+import { Alert, StyleSheet, Text, View } from "react-native";
+import { z } from "zod";
+
+const formSchema = z.object({
+  email: z.string().email("Please enter a valid email"),
+  password: z.string().min(6, "Password must be at least 6 characters"),
+  confirmPassword: z.string().min(6, "Password must be at least 6 characters"),
+});
+
+const RegisterScreen = () => {
+  const { control, handleSubmit } = useForm({
+    defaultValues: {
+      email: "",
+      password: "",
+      confirmPassword: "",
+    },
+    resolver: zodResolver(formSchema),
+    mode: "onBlur",
+    reValidateMode: "onBlur",
+  });
+const onSubmit = (formData: { email: string, password: string, confirmPassword: string }) => {
+    if (formData.password !== formData.confirmPassword){
+        Alert.alert("Passwords don't match");
+        return;
+    }
+    if (formData.email === "admin@liceolapaz.net") {
+        Alert.alert("Account already exists");
+    } else {
+      router.push("/teams");
+    }
+  };
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Register</Text>
+      <FormInput
+        control={control}
+        name="email"
+        autoCapitalize="none"
+        autoCompleteType="email"
+        inputMode="email"
+        placeholder="Enter your email address"
+      />
+      <FormInput
+        control={control}
+        name="password"
+        autoCapitalize="none"
+        inputMode="text"
+        placeholder="Enter your password"
+        secureTextEntry
+      />
+      <FormInput
+        control={control}
+        name="confirmPassword"
+        autoCapitalize="none"
+        inputMode="text"
+        placeholder="Confirm your password"
+        secureTextEntry
+      />
+      <Button text="Login" onPress={handleSubmit(onSubmit)} />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+    gap: 20,
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: "bold",
+  },
+});
+
+export default RegisterScreen;
